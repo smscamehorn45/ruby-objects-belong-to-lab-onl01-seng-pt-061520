90d206b1a8e42695a374ff0964c720122e13c9ce
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-13 22:39:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,3 +12,13 @@ drake = Artist.new("Drake", "rap")
 hotline_bling = Song.new("Hotline Bling")
  
 hotline_bling.artist = drake
+
+class Song
+ 
+  attr_accessor :title, :artist
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+end
